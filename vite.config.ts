916edf893bc69f1ce5da4b7c66840b1b24d78203
Fernando Silva <fernandosilvadj@hotmail.com>
2022-09-22 01:53:11 -0300

    chore: config tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -6,5 +9,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTest.ts",
   },
 });
