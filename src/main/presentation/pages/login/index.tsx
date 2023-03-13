43e8cc86095b1ca2871e6f678d8a33643d99278a
Fernando Silva <fernandosilvadj@hotmail.com>
2023-03-13 06:05:59 -0300

    feat: add login page

---
+++ src/main/presentation/pages/login/index.tsx
@@ -0,0 +1,43 @@
+import { Grid, Typography, Box, TextField, Button } from "@mui/material";
+import Background from "./assets/background.jpg";
+import * as S from "./styles";
+
+export function Login() {
+  return (
+    <Grid container>
+      <Grid item xs={7}>
+        <S.BoxImageContainer>
+          <img
+            src={Background}
+            alt='Imagem de um computador com códigos na tela'
+          />
+        </S.BoxImageContainer>
+      </Grid>
+
+      <Grid item xs={5}>
+        <S.BoxFormContainer component='form'>
+          <Typography variant='h2' color='initial'>
+            Entrar
+          </Typography>
+          <TextField
+            className='form__input'
+            id='Email'
+            label='Email'
+            variant='standard'
+            type='email'
+          />
+          <TextField
+            className='form__input'
+            id='Password'
+            label='Password'
+            variant='standard'
+            type='password'
+          />
+          <Button className='form__button' variant='contained' type='submit'>
+            Entrar
+          </Button>
+        </S.BoxFormContainer>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ src/main/presentation/pages/login/styles.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const BoxImageContainer = styled(Box)(() => ({
+  "& img": {
+    maxWidth: "100%",
+    height: "100vh",
+  },
+}));
+
+export const BoxFormContainer = styled(Box)(() => ({
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+
+  "& .form": {
+    "&__input": {
+      width: "80%",
+      marginBottom: 20,
+    },
+
+    "&__button": {
+      marginTop: 20,
+    },
+  },
+}));
